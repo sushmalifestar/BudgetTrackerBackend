feb65907e3dc2740ee65bc16c4e0f05cee0e02fa
Sai Sushma Cherukupalli <sushmalifestar@gmail.com>
2026-06-20 14:20:55 +0530

    testing change

--- config/db.config.js
+++ config/db.config.js
@@ -18,6 +18,7 @@ console.log("DB_USER =", process.env.DB_USER);
 console.log("DB_SERVER =", process.env.DB_SERVER);
 console.log("DB_NAME =", process.env.DB_NAME);
 console.log("DB_PASSWORD exists =", process.env.DB_PASSWORD);
+console.log("DB_PASSWORD length =", process.env.DB_PASSWORD?.length);
 
 module.exports = {
     sql,
