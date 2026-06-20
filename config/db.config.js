f042c783288190fe12fb4f87b4974b09439caec9
Sai Sushma Cherukupalli <sushmalifestar@gmail.com>
2026-06-20 13:21:06 +0530

    config Changes by Sreenivas

--- config/db.config.js
+++ config/db.config.js
@@ -7,6 +7,7 @@ const config = {
     server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
     options: {
+        encrypt: true,
         trustServerCertificate: true
     }
 };
@@ -14,7 +15,7 @@ const config = {
 console.log("DB_USER =", process.env.DB_USER);
 console.log("DB_SERVER =", process.env.DB_SERVER);
 console.log("DB_NAME =", process.env.DB_NAME);
-console.log("DB_PASSWORD exists =", !!process.env.DB_PASSWORD);
+console.log("DB_PASSWORD exists =", process.env.DB_PASSWORD);
 
 module.exports = {
     sql,
